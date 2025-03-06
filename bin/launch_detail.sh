a024fa50c2bf123089f019e44913cbabcf97754e
Kiryl Anatsyeu <kirio.anatsyeu@gmail.com>
2025-03-06 20:17:26 +0300

    Доработка раздела db_create

--- bin/launch_detail.sh
+++ bin/launch_detail.sh
@@ -9,15 +9,15 @@ db_settings='| PGPASSWORD=$pgpswd psql -d mydb'
 # 1.1. Проверка источника Slava
 etl_id=20
 # проверка по дате изменений в stage
-RESULT=$(echo "select date_part('day', loaded_dttm) = date_part('day', now()) from stage.sql_functions_slava limit 1;" | PGPASSWORD=$pgpswd psql -d mydb)
+RESULT=$(echo "select date_part('day', loaded_dttm) = date_part('day', now()) from stage.sql_functions_slava limit 1;" | PGPASSWORD=$pgpswd psql -d $db)
 array=($RESULT)
 today=${array[2]}
 # провверка по метке "finished" предыдущего процесса
-RESULT=$(echo "SELECT detail.check_etl_finish(19, 3)" | PGPASSWORD=$pgpswd psql -d mydb)
+RESULT=$(echo "SELECT detail.check_etl_finish(19, 3)" | PGPASSWORD=$pgpswd psql -d $db)
 array=($RESULT)
 stage_finished=${array[2]}
 # Проверка по метке "finished" теущего процесса (чтобы не гоняло по кругу)
-RESULT=$(echo "SELECT detail.check_etl_finish($etl_id, $step_num)" | PGPASSWORD=$pgpswd psql -d mydb)
+RESULT=$(echo "SELECT detail.check_etl_finish($etl_id, $step_num)" | PGPASSWORD=$pgpswd psql -d $db)
 array=($RESULT)
 detail_finished_slava=${array[2]} 
 
@@ -28,7 +28,7 @@ if [[ $today == 't' && $stage_finished == 't' && $detail_finished_slava != 't' ]
     src_cd=Slava
     echo "-----------------------------------------------------"
     echo "2. Загрузка данных из stage в detail"
-    source "$local_path/bin/detail.sh"
+    source "$local_path/etl_sql_functions_postgresql/bin/detail.sh"
     flag=1
 fi
 
@@ -39,11 +39,11 @@ RESULT=$(echo "select date_part('day', loaded_dttm) = date_part('day', now()) fr
 array=($RESULT)
 today=${array[2]}
 # провверка по метке "finished" предыдущего процесса
-RESULT=$(echo "SELECT detail.check_etl_finish(17, 3)" | PGPASSWORD=$pgpswd psql -d mydb)
+RESULT=$(echo "SELECT detail.check_etl_finish(17, 3)" | PGPASSWORD=$pgpswd psql -d $db)
 array=($RESULT)
 stage_finished=${array[2]}
 # Проверка по метке "finished" теущего процесса (чтобы не гоняло по кругу)
-RESULT=$(echo "SELECT detail.check_etl_finish($etl_id, $step_num)" | PGPASSWORD=$pgpswd psql -d mydb)
+RESULT=$(echo "SELECT detail.check_etl_finish($etl_id, $step_num)" | PGPASSWORD=$pgpswd psql -d $db)
 array=($RESULT)
 detail_finished_alena=${array[2]}  
 
@@ -54,22 +54,22 @@ if [[ $today == 't' && $stage_finished == 't' && $detail_finished_alena != 't' ]
     src_cd=Alena
     echo "-----------------------------------------------------"
     echo "2. Загрузка данных из stage в detail"
-    source "$local_path/bin/detail.sh"
+    source "$local_path/etl_sql_functions_postgresql/bin/detail.sh"
     flag=1
 fi
 
 # 1.3. Проверка источника Kirill
 etl_id=16
 # проверка по дате изменений в stage
-RESULT=$(echo "select date_part('day', loaded_dttm) = date_part('day', now()) from stage.sql_functions limit 1;" | PGPASSWORD=$pgpswd psql -d mydb)
+RESULT=$(echo "select date_part('day', loaded_dttm) = date_part('day', now()) from stage.sql_functions limit 1;" | PGPASSWORD=$pgpswd psql -d $db)
 array=($RESULT)
 today=${array[2]}
 # провверка по метке "finished" предыдущего процесса
-RESULT=$(echo "SELECT detail.check_etl_finish(15, 3)" | PGPASSWORD=$pgpswd psql -d mydb)
+RESULT=$(echo "SELECT detail.check_etl_finish(15, 3)" | PGPASSWORD=$pgpswd psql -d $db)
 array=($RESULT)
 stage_finished=${array[2]}
 # Проверка по метке "finished" теущего процесса (чтобы не гоняло по кругу)
-RESULT=$(echo "SELECT detail.check_etl_finish($etl_id, $step_num)" | PGPASSWORD=$pgpswd psql -d mydb)
+RESULT=$(echo "SELECT detail.check_etl_finish($etl_id, $step_num)" | PGPASSWORD=$pgpswd psql -d $db)
 array=($RESULT)
 detail_finished_kirill=${array[2]} 
 
@@ -80,7 +80,7 @@ if [[ $today == 't' && $stage_finished == 't' && $detail_finished_kirill != 't' 
     src_cd=Kirill
     echo "-----------------------------------------------------"
     echo "2. Загрузка данных из stage в detail"
-    source "$local_path/bin/detail.sh"
+    source "$local_path/etl_sql_functions_postgresql/bin/detail.sh"
     flag=1
 fi
 
