e5613978877b173cd893034cd132878d387051ba
Kiryl Anatsyeu <kirio.anatsyeu@gmail.com>
2025-03-06 20:17:26 +0300

    Доработка раздела db_create

--- bin/stage.sh
+++ bin/stage.sh
@@ -3,7 +3,7 @@
 # 1. Начало процесса
     step_num=1
 # вызов процесса начала заполнения таблицы etl_run и придание переменной etl_run_id значение id    
-    RESULT=$(echo "SELECT detail.start_etl_run_f($etl_id, $step_num)" | PGPASSWORD=$pgpswd psql -d mydb)
+    RESULT=$(echo "SELECT detail.start_etl_run_f($etl_id, $step_num)" | PGPASSWORD=$pgpswd psql -d $db)
     array=($RESULT)
     etl_run=${array[2]:0:100}
     echo "etl_run_id:  "$etl_run
@@ -12,13 +12,13 @@
 # 2. Очистка слоя stage перед загрузкой туда новых данных
     step_num=2
 # вызов процесса начала заполнения таблицы etl_run и придание переменной etl_run_id значение id
-    RESULT=$(echo "SELECT detail.start_etl_run_f($etl_id, $step_num)" | PGPASSWORD=$pgpswd psql -d mydb)
+    RESULT=$(echo "SELECT detail.start_etl_run_f($etl_id, $step_num)" | PGPASSWORD=$pgpswd psql -d $db)
     array=($RESULT)
     etl_run=${array[2]:0:100}    
     PGPASSWORD=$pgpswd psql -d mydb -c "TRUNCATE $table_name;" 
     echo "etl_run_id:  "$etl_run
 # проверка выполнения очистки
-    RESULT=$(echo "select count(*) from $table_name" | PGPASSWORD=$pgpswd psql -d mydb)
+    RESULT=$(echo "select count(*) from $table_name" | PGPASSWORD=$pgpswd psql -d $db)
     array=($RESULT)
     str_cnt=${array[2]:0:100}
     echo $str_cnt
@@ -30,16 +30,16 @@
 # 3. Загрузка данных из csv файла в слой stage
         step_num=3
 # вызов процесса начала заполнения таблицы etl_run и придание переменной etl_run_id значение id
-        RESULT=$(echo "SELECT detail.start_etl_run_f($etl_id, $step_num)" | PGPASSWORD=$pgpswd psql -d mydb)
+        RESULT=$(echo "SELECT detail.start_etl_run_f($etl_id, $step_num)" | PGPASSWORD=$pgpswd psql -d $db)
         array=($RESULT)
         etl_run=${array[2]} 
         echo "etl_run_id:  "$etl_run           
-        PGPASSWORD=$pgpswd psql -d mydb -c "\COPY $table_name($fields) FROM '$local_path/etl_files/$csv' WITH (FORMAT CSV, HEADER);" 
+        PGPASSWORD=$pgpswd psql -d mydb -c "\COPY $table_name($fields) FROM '$local_path/etl_sql_functions_postgresql/etl_files/$csv' WITH (FORMAT CSV, HEADER);" 
 # проверка выполнения копирования
-        RESULT=$(echo "select count(*) from $table_name" | PGPASSWORD=$pgpswd psql -d mydb)
+        RESULT=$(echo "select count(*) from $table_name" | PGPASSWORD=$pgpswd psql -d $db)
         array=($RESULT)
         str_cnt=${array[2]}
-        RESULT=$(wc -l $local_path"/etl_files/$csv")
+        RESULT=$(wc -l $local_path"/etl_sql_functions_postgresql/etl_files/$csv")
         array=($RESULT)
         str_cnt_csv=${array[0]}
         let "str_cnt_csv = $str_cnt_csv-1"
